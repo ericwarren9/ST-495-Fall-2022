cb46c1254b11ce838a5a028b4a87103e8728108a
ericwarren9 <erwarren@ncsu.edu>
2022-08-31 16:14:53 -0400

    ST 495 Week 2 Lectures

--- EDA 8:29:22.R
+++ EDA 8:29:22.R
@@ -70,3 +70,77 @@ names(cityDF)[1] <- "City"
 
 #Find top 5 cities for crashes
 cityDF[2:6, ]
+
+
+# Visualization -----------------------------------------------------------
+
+bicycleCrash %>%
+  ggplot(aes(x = Region,
+             y = Bike_Age,
+             fill = Region)) +
+  geom_boxplot() +
+  theme_bw()
+
+bicycleCrash %>%
+  ggplot(aes(x = Region,
+             y = Drvr_Age,
+             fill = Region)) +
+  geom_boxplot() +
+  theme_bw()
+
+bicycleCrash %>%
+  ggplot(aes(x = BikerAge_Group)) +
+  geom_bar() +
+  theme_bw()
+
+bicycleCrash %>%
+  ggplot(aes(x = DrvrAge_Gr)) +
+  geom_bar() +
+  theme_bw()
+
+# Stacked barplot
+bicycleCrash %>%
+  ggplot(aes(x = BikerAge_Group,
+             fill = Region)) +
+  geom_bar() +
+  theme_bw()
+
+bicycleCrash %>%
+  ggplot(aes(x = BikerAge_Group,
+             fill = Rural_Urba)) +
+  geom_bar() +
+  theme_bw()
+
+
+# Back to data cleaning ---------------------------------------------------
+
+class(bicycleCrash$CrashDay)
+table(bicycleCrash$CrashDay)
+length(unique(bicycleCrash$CrashDay))
+weekdays(as.Date(as.character(bicycleCrash$Crash_Date)))
+
+crashDay = rep(NA, nrow(bicycleCrash))
+for (i in 1:nrow(bicycleCrash)) {
+  crashDay[i] = weekdays(as.Date(as.character(bicycleCrash$Crash_Date[i])))
+}
+
+bicycleCrash$crashDay <- crashDay
+
+table(bicycleCrash$crashDay)
+
+# Speed Limits
+
+table(bicycleCrash$Speed_Limi)
+
+bicycleCrash$Speed_Limi[which(bicycleCrash$Speed_Limi == "")] = NA
+
+speedLimit <- factor(bicycleCrash$Speed_Limi,
+                     levels(bicycleCrash$Speed_Limi)[c(5, 2, 3, 4, 6, 7)])
+table(speedLimit)
+
+test$speedLimit <- speedLimit
+
+test %>%
+  ggplot(aes(x = speedLimit)) +
+  geom_bar() +
+  theme_bw()
