3a6a7069d7e76d74280a09549a583ddec729d60b
ericwarren9 <erwarren@ncsu.edu>
2022-09-14 20:51:19 -0400

    Linear Regression Class Notes

--- Linear Regression Lecture Notes.R
+++ Linear Regression Lecture Notes.R
@@ -1,5 +1,5 @@
 # Purpose: Linear Regression concepts with synthetic data
-
+library(tidyverse)
 
 # Make data ---------------------------------------------------------------
 
@@ -40,7 +40,7 @@ lmFit5 <- lm(y ~ x1 + x2 + x3 + z1)
 summary(lmFit5)
 
 
-# Load in new data --------------------------------------------------------
+# Examine Linear Regression with Boston data ------------------------------
 
 library(ISLR2)
 library(MASS)
@@ -69,3 +69,70 @@ predict(bostonFit,
 # Diagnostics
 par(mfrow = c(2, 2))
 plot(bostonFit)
+
+# Make another linear regression model
+bostonFit2 <- lm(medv ~ lstat + age, Boston)
+
+# Check Correlation
+cor(Boston$lstat, Boston$age)
+
+# Non-linear transformations
+bostonFit3 <- lm(medv ~ lstat + I(lstat^2), Boston)
+summary(bostonFit3)
+
+anova(bostonFit, bostonFit3)
+
+# Finding the best model
+allBostonFit <- lm(medv ~ ., Boston)
+noBostonFit <- lm(medv ~ 1, Boston)
+
+modelTesting <- stepAIC(noBostonFit, 
+                        scope = list(lower = noBostonFit,
+                                     upper = allBostonFit),
+                        direction = "both")
+
+bestBostonFit <- lm(formula(modelTesting), Boston)
+summary(bestBostonFit)
+
+
+# Cross-validation --------------------------------------------------------
+
+set.seed(9)
+trainpct = 0.7
+n = nrow(Boston)
+train = sample(1:n,
+               round(n * trainpct))
+dataTrain <- Boston[train, ]
+dataTest <- Boston[-train, ]
+
+# Calibrate the model using training data
+bostonFitCV <- lm(formula(modelTesting), dataTrain)
+
+# Compute test errors 
+yhat1 = predict(bostonFitCV, newdata = dataTest)
+ytest = dataTest$medv
+sqrt(mean((yhat1 - ytest) ** 2))
+
+# Look at training error for comparison
+yhat2 = predict(bostonFitCV, newdata = dataTrain)
+ytrain = dataTrain$medv
+sqrt(mean((yhat2 - ytrain) ** 2))
+
+
+# Running Diagnostics for Linear Regression -------------------------------
+
+par(mfrow = c(2, 2))
+plot(bestBostonFit)
+
+# Leverage Scores
+par(mfrow = c(1, 1))
+lev = hat(model.matrix(bestBostonFit))
+fitted <- predict(bestBostonFit)
+plot(fitted, lev)
+p = 11
+n = nrow(Boston)
+which(lev > 10 * p / n) # Rule of thumb: 10*p/n
+
+# Cook's distance
+cooks <- cooks.distance(bestBostonFit) # Rule of thumb: 4/n or 4/(n-p-1)
+which(cooks == max(cooks))
