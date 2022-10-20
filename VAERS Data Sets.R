b833f524f0b5056150fcd49711118f264129f490
ericwarren9 <erwarren@ncsu.edu>
2022-10-20 14:10:06 -0400

    File to clean the VAERS Data

--- VAERS Data Sets.R
+++ VAERS Data Sets.R
@@ -5,20 +5,26 @@
 
 library(tidyverse)
 
-files <- list.files("~/ST-495-Fall-2022/FinalProjectRawData")
+filesData <- list.files("~/ST-495-Fall-2022/FinalProjectRawData/RegularData")
 
-setwd("~/ST-495-Fall-2022/FinalProjectRawData")
+setwd("~/ST-495-Fall-2022/FinalProjectRawData/RegularData")
 
-vaersData <- bind_rows(lapply(files, read_csv))
+vaersData <- bind_rows(lapply(filesData, read_csv))
+
+filesSymptoms <- list.files("~/ST-495-Fall-2022/FinalProjectRawData/SymptomData")
+
+setwd("~/ST-495-Fall-2022/FinalProjectRawData/SymptomData")
+
+symptomData <- bind_rows(lapply(filesSymptoms, read_csv))
 
 setwd("~/ST-495-Fall-2022")
 
 
 # Manipulate data how needed ----------------------------------------------
 
-
+# Do VAERS Data Here
 vaersDataUpdated <- vaersData %>%
-  select(-VAERS_ID) %>%
+  select(-SYMPTOM_TEXT) %>%
   mutate(RECVDATE = as.Date(RECVDATE, tryFormats = "%m/%d/%Y"),
          RPT_DATE = as.Date(RPT_DATE, tryFormats = "%m/%d/%Y"),
          DATEDIED = as.Date(DATEDIED, tryFormats = "%m/%d/%Y"),
@@ -39,10 +45,62 @@ vaersDataUpdated <- vaersData %>%
          OFC_VISIT = ifelse(is.na(OFC_VISIT), "N", OFC_VISIT)
          )
 
+# Do Symptoms Data Here
+symptomDataSelected <- symptomData %>%
+  dplyr::select(VAERS_ID, SYMPTOM1, SYMPTOM2, SYMPTOM3, SYMPTOM4, SYMPTOM5)
+
+symptomDataUpdated <- symptomDataSelected %>%
+  group_by(VAERS_ID) %>%
+  mutate(SYMPTOM_TEXT = paste(c(SYMPTOM1, SYMPTOM2, SYMPTOM3, SYMPTOM4, SYMPTOM5), collapse = ", ")) %>%
+  dplyr::select(VAERS_ID, SYMPTOM_TEXT) %>%
+  ungroup() 
+
+# Clean the columns up
+symptomDataUpdated <- symptomDataUpdated[! duplicated(symptomDataUpdated), ]
+symptomDataUpdated$SYMPTOM_TEXT <- gsub("NA, ", "", symptomDataUpdated$SYMPTOM_TEXT)
+symptomDataUpdated$SYMPTOM_TEXT <- gsub(", NA", "", symptomDataUpdated$SYMPTOM_TEXT)
+symptomDataUpdated$SYMPTOM_TEXT <- ifelse(grepl("No adverse event", symptomDataUpdated$SYMPTOM_TEXT), 'None', symptomDataUpdated$SYMPTOM_TEXT)
+
+# Merge Data Into One File
+finalVAERSData <- merge(vaersDataUpdated, symptomDataUpdated, by = "VAERS_ID") # Note there are 5 people who are in the regular data file but did not get the vaccine so we excluded them.
+finalVAERSData <- finalVAERSData %>%
+  dplyr::select(-VAERS_ID)
+
+
+# Separate Data By Years --------------------------------------------------
+
+n1 <- as.Date("2017-01-01")
+n2 <- as.Date("2018-01-01")
+n3 <- as.Date("2019-01-01")
+n4 <- as.Date("2020-01-01")
+n5 <- as.Date("2021-01-01")
+
+finalVAERSData2016 <- finalVAERSData %>%
+  filter(RECVDATE < n1)
+finalVAERSData2017 <- finalVAERSData %>%
+  filter(between(RECVDATE, n1, n2-1))
+finalVAERSData2018 <- finalVAERSData %>%
+  filter(between(RECVDATE, n2, n3-1))
+finalVAERSData2019 <- finalVAERSData %>%
+  filter(between(RECVDATE, n3, n4-1))
+finalVAERSData2020 <- finalVAERSData %>%
+  filter(between(RECVDATE, n4, n5-1))
+finalVAERSData2021 <- finalVAERSData %>%
+  filter(RECVDATE >= n5)
 
 
 # Make files of new data --------------------------------------------------
 
-write_csv(vaersDataUpdated, "~/ST-495-Fall-2022/FinalProjectUsedData/2016-21VAERSData.csv")
+write_csv(finalVAERSData, "~/ST-495-Fall-2022/FinalProjectUsedData/2016-21VAERSData.csv") #Full data set
 
-write_rds(vaersDataUpdated, "~/ST-495-Fall-2022/FinalProjectUsedData/2016-21VAERSData.rds")
+write_csv(finalVAERSData2016, "~/ST-495-Fall-2022/FinalProjectUsedData/2016VAERSData.csv") #2016 data set
+
+write_csv(finalVAERSData2017, "~/ST-495-Fall-2022/FinalProjectUsedData/2017VAERSData.csv") #2017 data set
+
+write_csv(finalVAERSData2018, "~/ST-495-Fall-2022/FinalProjectUsedData/2018VAERSData.csv") #2018 data set
+
+write_csv(finalVAERSData2019, "~/ST-495-Fall-2022/FinalProjectUsedData/2019VAERSData.csv") #2019 data set
+
+write_csv(finalVAERSData2020, "~/ST-495-Fall-2022/FinalProjectUsedData/2020VAERSData.csv") #2020 data set
+
+write_csv(finalVAERSData2021, "~/ST-495-Fall-2022/FinalProjectUsedData/2021VAERSData.csv") #2021 data set
